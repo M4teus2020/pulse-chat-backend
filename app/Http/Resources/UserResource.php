4733c1487ae547a305a12c84afe441d6407f0c33
Mateus <mfelini7@gmail.com>
2025-01-12 22:04:42 -0300

    ✨ feat: enhance UserResource to include user image and generate capitalized name initials

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,7 +16,16 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'image' => null,
+            'cap_name' => $this->getCapName(),
             'created_at' => $this->created_at,
         ];
+    }
+
+    private function getCapName(): string
+    {
+        $names = explode(' ', $this->name);
+        $initials = array_map(fn($name) => substr($name, 0, 1), $names);
+        return implode('', array_slice($initials, 0, 3));
     }
 }
